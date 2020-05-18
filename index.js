5489b7221b38c053f3c0281a8e26653eaea30b9d
gaurav3210 <gs070599@gmail.com>
2020-05-18 14:13:41 +0530

    Added playSound function

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+//alert("Hello");
+//$("h1").text("Triggerd jquery");
+var buttonColors = ['red','blue','green','yellow'];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+var gameStart = false;
+
+function playSound(name){
+  var audio = new Audio("sounds/"+name+".mp3");
+  audio.play();
+
+}
+
 function newSequence(){
+  userClickedPattern = [];
+  level+=1;
+   $("#level-title").text("LEVEL "+ level);
+   var randomNumber = Math.floor(Math.random()*4);
+   var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+ $("#"+randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+ playSound(randomChosenColor);
+
+}
